3f8fafd09b992936c7c0eac8b7ecf7ef1425e926
Toymaker1010 <kiyokyo131214@gmail.com>
2024-04-20 11:28:16 +0900

    適切なコミットコメント

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
+        Paginator::useBootstrap();    //動画
+        
+        // Paginator::useBootstrapFive();    公式ドキュメント
+       //または Paginator::useBootstrapFour();    公式ドキュメント
         //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\PostController;
-Route::get('/posts', [PostController::class, 'index']);   
+*/
+Route::get('/', [PostController::class, 'index']);   
+
